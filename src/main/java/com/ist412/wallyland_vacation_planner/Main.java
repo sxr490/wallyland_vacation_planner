907c8ac521d3930dc66c1d0d5e89000bc143352c
NucciTheBoss <jcn23@psu.edu>
2021-11-03 11:28:48 -0400

    Add initial files

---
+++ src/main/java/com/ist412/wallyland_vacation_planner/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ist412.wallyland_vacation_planner;
+
+/**
+ * Main class that serves as entry point for RootView
+ * 
+ * @author Jason C. Nucciarone
+ */
+public class Main {
+   
+}
---
+++ src/main/java/com/ist412/wallyland_vacation_planner/login/Login.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ist412.wallyland_vacation_planner.login;
+
+/**
+ *
+ * @author jason
+ */
+public class Login {
+    
+}
