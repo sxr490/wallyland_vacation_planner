76b2d8b1b972e153e3106acdd78252daa0878190
sxr490 <66664051+sxr490@users.noreply.github.com>
2021-11-07 23:47:14 -0500

    Update PlanTripModel.java

--- src/main/java/com/ist412/wallyland_vacation_planner/planning_trip/PlanTripModel.java
+++ src/main/java/com/ist412/wallyland_vacation_planner/planning_trip/PlanTripModel.java
@@ -4,10 +4,58 @@
  */
 package com.ist412.wallyland_vacation_planner.planning_trip;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jason
  */
 public class PlanTripModel {
+    
+    private ArrayList<String> freeDates; 
+    private ArrayList<String> bookedDates; 
+    
+    
+    
+    public PlanTripModel(){
+    
+    }
+    
+    public void bookDate(String date, String name)
+    {
+        boolean remove = false; 
+        int index = 0; 
+        for(int k = 0; k < freeDates.size(); k++){
+            if (freeDates.get(k) == date){
+                remove = true; 
+                index = k; 
+            }
+        }
+        
+        if(remove == true){
+            freeDates.remove(index); 
+            bookedDates.add(date);
+        }
+    }
+    
+    public void cancelTrip(String date, String name)
+    {
+        boolean remove = false; 
+        int index = 0; 
+        for(int k = 0; k < freeDates.size(); k++){
+            if (freeDates.get(k) == date){
+                remove = true; 
+                index = k; 
+            }
+        }
+        
+        if(remove == true){
+            bookedDates.remove(index); 
+            freeDates.add(date); 
+        }
+    }
+    
+    
+    
     
 }
